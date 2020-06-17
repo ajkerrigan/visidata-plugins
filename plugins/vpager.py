467a67abcddf553de68f4086a06d82afed801caa
AJ Kerrigan <aj@speckledmonkey.com>
2020-06-17 09:49:30 -0400

    Suspend curses in vpager
    Behave more like launchEditor(). Avoid terminal tomfoolery.

--- plugins/vpager.py
+++ plugins/vpager.py
@@ -2,14 +2,14 @@ import os
 import subprocess
 from shutil import which
 
-from visidata import BaseSheet, Column, vd
+from visidata import BaseSheet, Column, SuspendCurses, vd
 
 
 @Column.api
 def pageValue(col, row):
     pager = os.environ.get('PAGER', which('less'))
-    subprocess.run(pager, input=str(col.getValue(row)), encoding='utf8')
-    vd.redraw()
+    with SuspendCurses():
+        return subprocess.run(pager, input=str(col.getValue(row)), encoding='utf8')
 
 
 BaseSheet.addCommand(
