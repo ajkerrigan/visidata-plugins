f4a6d9ec4217f00b730612072691ad0236a68912
AJ Kerrigan <aj@speckledmonkey.com>
2020-06-17 09:30:04 -0400

    Make vpager part of the Column API

--- plugins/vpager.py
+++ plugins/vpager.py
@@ -2,15 +2,16 @@ import os
 import subprocess
 from shutil import which
 
-from visidata import BaseSheet, vd
+from visidata import BaseSheet, Column, vd
 
 
-def page(col, row):
+@Column.api
+def pageValue(col, row):
     pager = os.environ.get('PAGER', which('less'))
     subprocess.run(pager, input=str(col.getValue(row)), encoding='utf8')
     vd.redraw()
 
 
 BaseSheet.addCommand(
-    "z^O", "syspager-cell", "plugins.vpager.page(cursorCol, cursorRow)"
+    "z^O", "syspager-cell", "cursorCol.pageValue(cursorRow)"
 )
