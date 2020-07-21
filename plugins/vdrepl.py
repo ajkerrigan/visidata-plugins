b4ee03085d106166f1153055335f2f8a3d273f1a
AJ Kerrigan <aj@speckledmonkey.com>
2020-07-21 03:30:09 -0400

    [vdrepl] store history under visidata_dir

--- plugins/vdrepl.py
+++ plugins/vdrepl.py
@@ -4,13 +4,19 @@ Launch an embedded ptipython REPL from within VisiData.
 
 import sys
 
+from pathlib import Path
+
 from ptpython.ipython import embed, InteractiveShellEmbed
+from prompt_toolkit.history import ThreadedHistory, FileHistory
 from visidata import LazyChainMap, Sheet, SuspendCurses, VisiData, vd
+
 
 class Dummy:
     '''Hacks to patch sheet-local variables that we want the REPL to ignore.'''
+
     replayStatus = 'dummy'
     someSelectedRows = 'dummy'
+
 
 @VisiData.api
 def openRepl(self):
@@ -28,17 +34,23 @@ def openRepl(self):
         try:
             sys.stdin = vd._stdin
             sys.stdout = open('/dev/tty', mode='w')
-            shell = InteractiveShellEmbed.instance()
+            history_file = (
+                Path(vd.options.visidata_dir).expanduser()
+                / 'cache'
+                / 'ptpython'
+                / 'history'
+            )
+            Path.mkdir(history_file.parent, parents=True, exist_ok=True)
+            shell = InteractiveShellEmbed.instance(
+                history_filename=str(history_file), vi_mode=True,
+            )
+            shell.python_input.title = 'VisiData IPython REPL (ptipython)'
             shell.python_input.show_exit_confirmation = False
-            shell.python_input.vi_mode = True
             embed()
-            shell.reset()
         except Exception as e:
             vd.exceptionCaught(e)
         finally:
             sys.stdin = orig_stdin
             sys.stdout = orig_stdout
 
-Sheet.addCommand(
-    "gz^X", "open-repl", "vd.openRepl()"
-)
+Sheet.addCommand("gz^X", "open-repl", "vd.openRepl()")
