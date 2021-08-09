f03338adabc9914cf37d41ad06fd1494e43a8114
AJ Kerrigan <aj@speckledmonkey.com>
2021-08-09 00:58:37 -0400

    Add JMESPath plugin
    Add support for creating new columns or selecting/unselecting rows based
    on JMESPath expressions. Perhaps useful or interesting to folks
    exploring AWS CLI queries, or more advanced Cloud Custodian value
    filters.
    
    See also:
    
    https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-filter.html
    https://cloudcustodian.io/docs/filters.html#value-filter
    https://jmespath.org

---
+++ plugins/jmespath.py
@@ -0,0 +1,43 @@
+from functools import partial
+import jmespath
+from visidata import BaseSheet, ExprColumn
+
+
+@BaseSheet.api
+def addcol_jmespath(sheet):
+    expr = sheet.inputExpr("new column jmespath expression=")
+    sheet.addColumnAtCursor(
+        ExprColumn(
+            expr,
+            expr=f"jmespath.search('{expr}', row)",
+            curcol=sheet.cursorCol
+        )
+    )
+
+@BaseSheet.api
+def select_by_jmespath(sheet, unselect=False):
+    action = 'unselect' if unselect else 'select'
+    expr = sheet.inputExpr(f"{action} by jmespath expression=")
+
+    match_func = partial(jmespath.search, expr)
+    select_func = getattr(sheet, action)
+    select_func(sheet.gatherBy(match_func), progress=False)
+
+BaseSheet.addCommand(
+    '',
+    'addcol-jmespath',
+    'sheet.addcol_jmespath()',
+    'create new column from a jmespath expression'
+)
+BaseSheet.addCommand(
+    '',
+    'select-jmespath',
+    'sheet.select_by_jmespath()',
+    'select rows matching a jmespath expression in any visible column'
+)
+BaseSheet.addCommand(
+    '',
+    'unselect-jmespath',
+    'sheet.select_by_jmespath(unselect=True)',
+    'unselect rows matching a jmespath expression in any visible column'
+)
