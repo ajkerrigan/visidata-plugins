0d7732337628034b0994f7c93e6dadbe106206c5
AJ Kerrigan <aj@speckledmonkey.com>
2021-11-19 15:12:55 -0500

    [vpager] add a vd option for specifying a pager
    * Mucking with the environment from VisiData is suboptimal, so use a vd
      option that falls back to $PAGER
    
    * Handle pager commands with arguments
    
    * Don't bind a key for `syspager-cell`

--- plugins/vpager.py
+++ plugins/vpager.py
@@ -1,17 +1,19 @@
 import os
+import shlex
 import subprocess
 from shutil import which
 
 from visidata import BaseSheet, Column, SuspendCurses, vd
 
+vd.option("vpager_cmd", "", "external command for displaying cell contents")
+
 
 @Column.api
 def pageValue(col, row):
-    pager = os.environ.get('PAGER', which('less'))
+    pager = vd.options.vpager_cmd or os.environ.get("PAGER", which("less"))
     with SuspendCurses():
-        return subprocess.run(pager, input=str(col.getValue(row)), encoding='utf8')
+        return subprocess.run(
+            shlex.split(pager), input=str(col.getValue(row)), encoding="utf8"
+        )
 
-
-BaseSheet.addCommand(
-    "z^O", "syspager-cell", "cursorCol.pageValue(cursorRow)"
-)
+BaseSheet.addCommand("", "syspager-cell", "cursorCol.pageValue(cursorRow)")
